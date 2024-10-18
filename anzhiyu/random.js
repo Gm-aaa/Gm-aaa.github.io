4f60f57162635fe7dd63c709ef9fcda286adeb67
G <gmasterm1m@gmail.com>
2024-10-19 06:59:35 +0800

    Site updated: 2024-10-19 06:59:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/18/RocketMQ初识消息队列(1)/","2024/10/18/RocketMQ的部署和测试(2)/"];function toRandomPost(){
+var posts=["2024/10/18/RocketMQ的部署和测试(2)/","2024/10/18/RocketMQ初识消息队列(1)/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
