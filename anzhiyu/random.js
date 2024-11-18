cdba0be51954b4e9be619607598400d631932671
DESKTOP-DM07HIU\Administrator <gmasterm1m@gmail.com>
2024-11-18 12:38:34 +0800

    Site updated: 2024-11-18 12:38:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/16/2024-11-16-对于重装系统的一些心得/","2024/10/18/RocketMQ初识消息队列(1)/","2024/10/25/SpringCloudAlibaba 之 Nacos 服务发现/","2024/10/20/Dockerfile构建Dockerimage/","2024/10/18/RocketMQ的部署和测试(2)/","2024/11/18/SpringCloudAlibaba之Sentinel流量控制 /"];function toRandomPost(){
+var posts=["2024/11/16/2024-11-16-对于重装系统的一些心得/","2024/10/20/Dockerfile构建Dockerimage/","2024/10/18/RocketMQ初识消息队列(1)/","2024/10/25/SpringCloudAlibaba 之 Nacos 服务发现/","2024/11/18/SpringCloudAlibaba之Sentinel流量控制 /","2024/10/18/RocketMQ的部署和测试(2)/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
