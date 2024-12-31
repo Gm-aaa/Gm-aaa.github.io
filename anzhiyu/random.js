b9d364d83159398ff3f5a0722aaf34af4167a1a5
Gm-aaa <gmasterm1m@gmail.com>
2025-01-01 02:20:53 +0800

    Site updated: 2025-01-01 02:20:52

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["2024/11/16/2024-11-16-对于重装系统的一些心得/","2024/12/06/Debian12同步onedrive-googledrive-s3的解决方案/","2024/10/20/Dockerfile构建Dockerimage/","2024/10/18/RocketMQ初识消息队列(1)/","2024/11/18/IDEA激活步骤/","2024/10/18/RocketMQ的部署和测试(2)/","2024/10/25/SpringCloudAlibaba 之 Nacos 服务发现/","2024/11/18/SpringCloudAlibaba之Sentinel流量控制/","2024/12/05/e5-2666v3装机体验/","2024/12/29/配置真正的“科学”上网环境/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
